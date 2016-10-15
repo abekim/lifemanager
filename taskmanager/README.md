746504a20b04438cdfccfdee167556ad9e8f2f92
Abe Kim <eunabekim@gmail.com>
2016-10-15 00:29:36 -0400

    reset repo

--- taskmanager/README.md
+++
@@ -1,4 +0,0 @@
-# Task Manager
-
-A core aspect of life managing is task management
-This feature is included as a module in the app
--- taskmanager/models.py
+++
@@ -1,5 +0,0 @@
-from taskmanager import db
-
-class Task(db.Document):
-    description = db.StringField(required=True)
-    completed = db.BooleanField(default=False)
